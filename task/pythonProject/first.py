050ab88eb8723a3f0c6646f3a07314ba753656ae
Little0witch <eeeee.nom@gmail.com>
2024-06-17 11:14:05 +0300

    'first'

---
+++ task/pythonProject/first.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+
+def get_subdirectories(directory):
+    directory_all = list(os.walk(directory))
+    for item in directory_all:
+        _, _,file_names = item
+    list_path = []
+    for file_name in file_names:
+        list_path.append(os.path.join(directory,file_name))
+    result = pd.DataFrame(list_path)
+    return (result)
+
+directory_path = r"C:\Users\Татьяна\Desktop\modsen\5 Flower Types Classification Dataset-1\5 Flower Types Classification Dataset\Lilly"
+subdirectories = get_subdirectories(directory_path)
+
+print(subdirectories)
