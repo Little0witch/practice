84107cd9b3e9eb654717dc874034fd90a3f55015
Little0witch <eeeee.nom@gmail.com>
2024-06-20 11:54:19 +0300

    show image

--- task/pythonProject/first.py
+++ task/pythonProject/first.py
@@ -1,23 +1,16 @@
 import hashlib
 import os
 import pandas as pd
-import cv2
-
-
-# def get_image(file_name):
-#     img = cv2.imread(file_name)
-#     cv2.imshow('tmp', img)
-#     cv2.waitKey(0)
+import matplotlib.pyplot as plt
+from PIL import Image
 
 def cal_hash(path_file):
     return hashlib.md5(open(path_file, 'rb').read()).hexdigest()
 
-
 def get_subdirectories(directory):
-    # директории, поддиректории, файлы
     directory_all = list(os.walk(directory))
     for item in directory_all:
-        dir_path, _, file_names = item  # название файлов
+        dir_path, _, file_names = item
     list_path = []
     for file_name in file_names:
         list_path.append(os.path.join(dir_path, file_name))
@@ -27,12 +20,33 @@ def get_subdirectories(directory):
 
 def group_hash(table_hashsum):
     grouped = table_hashsum.groupby(["Image Hash"], as_index=False)
-    for hash_sum, group in grouped:
-        # print("Image Hash:", hash_sum)
-        print(group)
-        print()  # Пустая строка между группами
+    duplicates = []
+    for _, group in grouped:
+        if len(group) > 1:
+            duplicates.append(group)
+    if duplicates:
+        duplicates_df = pd.concat(duplicates)
+        duplicates_df.reset_index(drop=True, inplace=True)
+        return duplicates_df
+    else:
+        return pd.DataFrame()
+
 
 directory_path = r"C:\Users\Татьяна\Desktop\modsen\5 Flower Types Classification Dataset-1\5 Flower Types Classification Dataset\tmp"
 subdirectories = get_subdirectories(directory_path)
-group_hash(subdirectories)
-# print(subdirectories)
+group_hash_result = group_hash(subdirectories)
+fig = plt.figure(figsize=(10, 6))
+num_images = len(group_hash_result)
+num_cols = min(num_images, 2)  # количество столбцов в сетке
+num_rows = (num_images + num_cols - 1) // num_cols  #  количество строк в сетке
+
+for i, (_, item) in enumerate(group_hash_result.iterrows()):
+    file_path = item['File Path']
+    img = Image.open(file_path)
+
+    ax = fig.add_subplot(num_rows, num_cols, i+1)
+    ax.imshow(img)
+    ax.set_title(f'Image {i+1}')
+
+plt.tight_layout()
+plt.show()
