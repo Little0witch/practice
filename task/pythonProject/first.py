0707af3f4cfc07a164c1784562b8e6ebdab02fd7
Little0witch <eeeee.nom@gmail.com>
2024-06-26 11:25:51 +0300

    Save to file an .xlsx

--- task/pythonProject/first.py
+++ task/pythonProject/first.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
+import openpyxl
 
 
 # Подсчет хэш-суммы изображения
@@ -47,6 +48,13 @@ def group_hash(table_hashsum):
         return pd.DataFrame()
 
 
+# Save the DataFrame duplicates to an .xlsx file in the current directory
+def write_in_exel(df, file_name):
+    current_dir = os.getcwd()
+    file_path = os.path.join(current_dir, file_name)
+    df.to_excel(file_path, sheet_name='dubl_img')
+
+
 input_directory_path = input()
 request_directory = []
 while 1:
@@ -56,11 +64,18 @@ while 1:
         request_directory.append(input_directory_path)
         input_directory_path = input()
 image_list = pd.concat([get_list_image(directory_path) for directory_path in request_directory], ignore_index=True)
+while True:
+    save_data = input("Сохранить данные в файл? y-yes, n-no\t")
+    print(save_data)
+    if not save_data in ['y', 'n']:
+        print('Ошибка ввода. Повторите ввод')
+    else:
+        break
 image_hash_list = get_list_hash(image_list)
 group_hash_result = group_hash(image_hash_list)
 count_images = len(group_hash_result)
 if count_images > 0:
-    num_cols = min(count_images,2)
+    num_cols = min(count_images, 2)
     num_rows = (len(group_hash_result) + num_cols - 1) // num_cols
     fig = plt.figure(figsize=(10, 6))
     axes = []
@@ -68,7 +83,7 @@ if count_images > 0:
         file_path = item['File Path']
         file_hash = item['Image Hash']
         img = Image.open(file_path)
-        ax = fig.add_subplot(num_rows, num_cols, i+1)
+        ax = fig.add_subplot(num_rows, num_cols, i + 1)
         ax.imshow(img)
         ax.set_title(f'Image {i + 1}')
         ax.axis('off')
@@ -77,5 +92,7 @@ if count_images > 0:
 
     plt.tight_layout()
     plt.show()
+    if save_data == 'y':
+        write_in_exel(group_hash_result, 'file_tmp.xlsx')
 else:
     print("Not found duplicate")
